1ba27e8a01a505fb3fc9b0ddfd8b6fbac70423ac
janChorny <janchorny@tut.by>
2022-02-25 17:04:47 +0300

    feat: add non-finished layout

---
+++ js/script.js
@@ -0,0 +1,70 @@
+"use strict" /* Современный режим языка JavaScript (java - строго типизированный объектно-ориентированный язык програмированияб, script в переводе - сценарий)*/
+/* // alert ("Let the killing begin");
+let admin;
+let name1;
+name1 = "Jan";
+admin = name1;
+alert(admin);
+
+let ourPlanet, currentUser;
+
+alert(`Нас ${5 +2}`); 
+
+let name = "Ilya";
+
+alert(`hello ${1}`); 
+
+alert(`hello ${"name"}`);
+
+alert(`hello ${name}`);
+
+let age = prompt('Ваш возраст', 32);
+alert(age);
+
+let hasHeEaten = confirm('Он поел?');
+alert(hasHeEaten);
+
+alert(`Мне будет ${Number(age) + 1} года в 2022 году`);
+
+alert(Number(null));
+alert(Number(undefined));
+alert(Number(NaN));
+alert(String(true));
+let true1 = true;
+true1 = String(true1);
+alert(typeof true1);
+alert("123" - undefined);
+alert("" + 1 + 0);//'10'
+alert("" - 1 + 0);//-1
+alert(true + false);// 1
+alert(6 / "3");//2
+alert("2" * "3");//6
+alert(4 + 5 + "px");//9px
+alert("$" + 4 + 5);//"$45"
+alert("4" - 2);//2
+alert("4px" - 2);//NaN
+alert(7 / 0);//NaN
+alert("  -9  " + 5);//"-9 5"
+alert("  -9  " - 5);//-14
+alert(null + 1);//1
+alert(undefined + );//NaN
+alert(" \t \n" - 2);//NaN
+
+let value = prompt('Введите число', 0);
+
+if (value > 0) {
+	alert(1);
+} else if (value < 0) {
+	alert(-1);
+} else {
+	alert(0);
+}
+
+
+let login = prompt('Ваша должность?', '');
+
+let message = (login == 'Сотрудник') ? 'Привет' :
+(login == 'Директор') ? 'Здравствуйте' :
+(login == '') ? 'Нужно представиться': 'А ты что здесь делаешь?';
+ alert(message);
+ */
